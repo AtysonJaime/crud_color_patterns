2b31b0b2d81db6a9c3329eff53fcf1276f168353
Atyson Jaime <atysonjaime@gmail.com>
2022-12-19 18:07:47 -0300

    :art: :lipstick: Change layouts pages

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .main-content
+    .body-content
+      Nuxt
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+
+  async mounted() {
+    await this.$store.dispatch('language/getLocate', this.language)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.main-content {
+  display: flex;
+  width: 100%;
+  background-color: var(--cream);
+}
+</style>
---
+++ layouts/loginPage.vue
@@ -0,0 +1,79 @@
+<template lang="pug">
+  .main-content
+    Nuxt
+</template>
+
+<script>
+export default {
+  name: 'LoginLayout',
+
+  async mounted() {
+    await this.$store.dispatch('language/getLocate', this.language)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.main-content {
+  font-family: 'Chivo Mono' !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 1.875rem;
+  width: 100%;
+  height: 100vh;
+  background: #2980b9
+    url('https://static.tumblr.com/03fbbc566b081016810402488936fbae/pqpk3dn/MRSmlzpj3/tumblr_static_bg3.png')
+    repeat 0 0;
+  -webkit-animation: 10s linear 0s normal none infinite animate;
+  -moz-animation: 10s linear 0s normal none infinite animate;
+  -ms-animation: 10s linear 0s normal none infinite animate;
+  -o-animation: 10s linear 0s normal none infinite animate;
+  animation: 10s linear 0s normal none infinite animate;
+}
+@-webkit-keyframes animate {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 500px 0;
+  }
+}
+
+@-moz-keyframes animate {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 500px 0;
+  }
+}
+
+@-ms-keyframes animate {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 500px 0;
+  }
+}
+
+@-o-keyframes animate {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 500px 0;
+  }
+}
+
+@keyframes animate {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 500px 0;
+  }
+}
+</style>
